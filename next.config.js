33283b6158962957b72f3af6d2c4703bf7c1b3a0
mwk900 <matiwozniak@icloud.com>
2026-02-16 15:25:07 +0000

    Switch to static export for Netlify

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com'
-      }
-    ]
-  }
+  output: "export",
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
